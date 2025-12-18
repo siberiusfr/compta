f828eb419b77356a8f4bb42ea78034ccd7652968
Aymen Mehri <med.aymen.mehri@gmail.com>
2025-12-18 20:26:07 +0700

    add librairies

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,30 @@
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import skipPrettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  // Désactive les fichiers inutiles
+  { ignores: ['dist/**', 'node_modules/**', 'public/**'] },
+
+  // Base TypeScript & Vue
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'], // ou 'flat/recommended' pour plus de rigueur
+
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    rules: {
+      // Tes règles personnalisées ici
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+
+  // Toujours à la fin pour désactiver les conflits de formatage
+  skipPrettier
+)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: 'src/components.d.ts',
+    }),
+  ],
 })
