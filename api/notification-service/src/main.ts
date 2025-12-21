f4f171d93d6e12c4fd76c3660382724d09070842
Aymen Mehri <med.aymen.mehri@gmail.com>
2025-12-22 02:53:49 +0700

    add swagger

--- api/notification-service/src/main.ts
+++ api/notification-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notification Service')
+    .setDescription('API documentation for the Notification Service')
+    .setVersion('1.0')
+    .addTag('notifications')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
