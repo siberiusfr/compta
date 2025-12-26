b8e9cf55c919666d0dbe8730be65ab1ccf950420
Aymen Mehri <med.aymen.mehri@gmail.com>
2025-12-26 21:44:12 +0700

    resolve problems

---
+++ api/gateway/src/main/java/tn/compta/gateway/config/WebPropertiesConfig.java
@@ -0,0 +1,18 @@
+package tn.compta.gateway.config;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration to expose WebProperties.Resources bean
+ * required by GlobalErrorWebExceptionHandler.
+ */
+@Configuration
+public class WebPropertiesConfig {
+
+  @Bean
+  public WebProperties.Resources resources() {
+    return new WebProperties.Resources();
+  }
+}
