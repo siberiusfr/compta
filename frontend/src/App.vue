65a89db7b6480206a91af2d6a89ed5f2143bc1e6
Aymen Mehri <med.aymen.mehri@gmail.com>
2026-01-08 22:17:37 +0700

    add @tanstack/vue-query-devtools vee-validate zod vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
+  <VueQueryDevtools />
 </template>
 
 <style scoped>
